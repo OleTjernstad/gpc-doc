03a0ab40460080d0c432012cb408798533a4becf
Ole Tjernstad <ott556@gmail.com>
2023-04-30 22:33:37 +0200

    submit data

--- src/pages/signup.tsx
+++ src/pages/signup.tsx
@@ -1,8 +1,34 @@
+import React, { useState } from "react";
+
+import { Button } from "../components/button";
 import Layout from "@theme/Layout";
-import React from "react";
 import { TextInput } from "../components/text-input";
 
 export default function Hello() {
+  const [name, setName] = useState<string>("");
+  const [email, setEmail] = useState<string>("");
+
+  async function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    setLoading(true);
+    console.log(e);
+    const res = await fetch("http://localhost:8787", {
+      body: JSON.stringify({
+        email,
+        name,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+      method: "POST",
+    });
+    const { error, success } = await res.json();
+    console.log(success);
+    setLoading(false);
+  }
+
+  const [loading, setLoading] = useState<boolean>(false);
+
   return (
     <Layout title="Påmelding" description="Påmelding av lag til GPC 2024">
       <div
@@ -28,20 +54,28 @@ export default function Hello() {
           <p>Vi anbefaler 2 - 5 personer pr lag.</p>
 
           <hr />
-          <TextInput
-            label="Lag navn"
-            name="teamName"
-            value=""
-            onChange={console.log}
-          />
+          <form onSubmit={handleSubmit}>
+            <TextInput
+              label="Lag navn"
+              name="teamName"
+              value={name}
+              onChange={setName}
+            />
 
-          <TextInput
-            label="Epost (til lag ansvarlig)"
-            name="email"
-            type="email"
-            value=""
-            onChange={console.log}
-          />
+            <TextInput
+              label="Epost (til lag ansvarlig)"
+              name="email"
+              type="email"
+              value={email}
+              onChange={setEmail}
+            />
+
+            <Button
+              label="Send inn påmelding"
+              type="submit"
+              loading={loading}
+            />
+          </form>
         </div>
       </div>
     </Layout>
